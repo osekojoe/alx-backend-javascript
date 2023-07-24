fe4c7aa551d91fbb8fc0e259d5b920cbee1c2d2b
jdev <20902932+osekojoe@users.noreply.github.com>
2023-07-24 22:07:45 +0200

    ESLint

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,7 @@
+// Import the createReportObject function from the correct file path.
+import createReportObject from './path-to-createReportObject.js';
+
+// Fix ESLint errors related to indentation, semicolons, and unused variables.
 export default function* createIteratorObject(report) {
   for (const department in report) {
     for (const employee of report[department]) {
