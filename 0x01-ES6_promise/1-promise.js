a8ed8756d299602d7094195711e84a7ab678cf5a
jdev <20902932+osekojoe@users.noreply.github.com>
2023-07-26 19:32:16 +0200

    ESLint

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,15 @@
 export default function getFullResponseFromAPI(success) {
-	return new Promise((resolve, reject) => {
-		if (success === true) {
-			const response = {
-				status: 200,
-				body: 'Success'
-			}
-			resolve(response);
-		} else {
-			const error = new Error("The fake API is not working currently");
-			reject(error);
-		}
-	});
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      const response = {
+        status: 200,
+        body: 'Success'
+      };
+      resolve(response);
+      } else {
+        const error = new Error("The fake API is not working currently");
+        reject(error);
+      }
+  });
 }
+
