fe0a37421e0b80e973f3847f580f356a94145beb
jdev <20902932+osekojoe@users.noreply.github.com>
2023-07-26 20:15:48 +0200

    code and ESLint fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -9,6 +9,6 @@ export default function handleProfileSignup() {
       console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
     .catch(() => {
-      console.error('Signup system offline');
+      console.log('Signup system offline');
     });
 }
