5531ea3c27bb5cd76bbc251b2f6482587c826fd9
jdev <20902932+osekojoe@users.noreply.github.com>
2023-07-26 20:58:48 +0200

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,23 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const result = [];
   try {
-    const [userResult, photoResult] = await Promise.allSettled([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName),
-    ]);
-    return ([
-      {
-        status: userResult.status,
-        value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason,
-      },
-      {
-        status: photoResult.status,
-        value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason,
-      },
-    ]);
-  } catch (error) {
-    console.error(error);
-    return [];
+    const user = await signUpUser(firstName, lastName);
+    result.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (err) {
+    result.push({
+      status: 'rejected',
+      value: `Error: ${fileName} cannot be processed`,
+    });
   }
+  return result;
 }
