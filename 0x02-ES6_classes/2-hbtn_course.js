8b4b6c7df27861e9813b79bbc76702ba5804c037
jdev <20902932+osekojoe@users.noreply.github.com>
2023-07-27 19:30:02 +0200

    syntax

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -13,7 +13,7 @@ export default class HolbertonCourse {
     if (typeof newName === 'string') {
       this._name = newName;
     } else {
-      throw new Error('Name must be a string.');
+      throw new Error('Name must be a string');
     }
   }
 
@@ -25,7 +25,7 @@ export default class HolbertonCourse {
     if (typeof newLength === 'number') {
       this._length = newLength;
     } else {
-      throw new Error('Length must be a number.');
+      throw new Error('Length must be a number');
     }
   }
 
@@ -37,7 +37,7 @@ export default class HolbertonCourse {
     if (Array.isArray(newStudents)) {
       this._students = newStudents;
     } else {
-      throw new Error('Students must be an array.');
+      throw new Error('Students must be an array');
     }
   }
 }
