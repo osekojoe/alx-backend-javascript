75db5c627709c0b1a5c1e60234a535b4a5269a10
jdev <20902932+osekojoe@users.noreply.github.com>
2023-08-01 18:16:04 +0200

    More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(groceriesMap) {
+  if (!(groceriesMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  groceriesMap.forEach((value, key) => {
+    if (value === 1) {
+      groceriesMap.set(key, 100);
+    }
+  });
+}
