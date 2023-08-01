223b9b771f0c8cc5f3d8bb8662dd6839135fce47
jdev <20902932+osekojoe@users.noreply.github.com>
2023-08-01 16:53:21 +0200

    Combine filter, map and find

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  if (!Array.isArray(studentList)) {
+    return [];
+  }
+
+  const updatedStudents = studentList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
+      return { ...student, grade };
+    });
+
+  return updatedStudents;
+}
