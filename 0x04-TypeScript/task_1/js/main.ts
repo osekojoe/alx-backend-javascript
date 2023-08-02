f880cc5c23b8875562f83d157ec892ac250acf30
jdev <20902932+osekojoe@users.noreply.github.com>
2023-08-02 17:15:08 +0200

    Let's build a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,9 @@
+// teacher interface
+interface Teacher {
+	readonly firstName: string,
+	readonly lastName: string,
+	fullTimeEmployee: boolean,
+	yearsOfExperience?: number,
+    location: string,
+	[anyproperty: string]: any
+}
