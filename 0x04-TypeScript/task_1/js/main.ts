0f9bfdbd0c46ad0b8f39d0b5f4a608d7a35e5011
jdev <20902932+osekojoe@users.noreply.github.com>
2023-08-02 17:31:40 +0200

    Printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -13,3 +13,13 @@ interface Directors extends Teacher {
 	numberOfReports: number,
 }
 
+// printing teachers
+interface printTeacherFunction {
+	(firstName: string,
+		lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (
+	firstName: string,
+	lastName: string
+): string => `${firstName.charAt(0)}. ${lastName}`;
