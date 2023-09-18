114e891202a6f8e8f6d059c869ea1ebfbc751c48
jdev <20902932+osekojoe@users.noreply.github.com>
2023-09-18 12:29:00 +0200

    Organize a complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,14 @@
+/**
+ * Link routes
+ */
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+const mapRoutes = (app) => {
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
+
+export default mapRoutes;
+module.exports = mapRoutes;
