35981e7d6b0d2368f74ff309eca0ec0f39556119
jdev <20902932+osekojoe@users.noreply.github.com>
2023-09-18 12:28:04 +0200

    small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,18 @@
+/**
+ * Create a small HTTP server using Express
+ */
+
+const express = require('express');
+
+const app = express();
+const PORT = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!\n');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
