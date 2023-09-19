c66152549d14a7014b1c002c04b10ce9ec43e4ce
jdev <20902932+osekojoe@users.noreply.github.com>
2023-09-19 16:54:57 +0200

    small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,18 +4,21 @@
 const http = require('http');
 
 // Create an HTTP server
-const app = http.createServer((req, res) => {
-  // Set the response header to indicate plain text content
-  res.setHeader('Content-Type', 'text/plain');
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
 
-  // Send the response body
-  res.end('Hello Holberton School!\n');
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
 
-// Listen on port 1245
-const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
 
 // Export the app variable
